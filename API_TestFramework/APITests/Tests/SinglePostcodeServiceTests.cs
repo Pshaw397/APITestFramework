ad03f39a44a8bd8849c8f889999d87712e85c160
Pshaw397 <phillip.a.shaw@googlemail.com>
2021-04-27 15:33:32 +0100

    Added initial passing tests

---
+++ API_TestFramework/APITests/Tests/SinglePostcodeServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using API_App;
+
+namespace APITests.Tests
+{
+    public class WhenTheSinglePostCodeServiceIsCalled_WithValidPostcodes
+    {
+        SinglePostcodeService _singlePostcodeService;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _singlePostcodeService = new SinglePostcodeService();
+            _singlePostcodeService.MakeRequest("EC2Y 5AS");
+        }
+
+        [Test]
+        public void StatusIs200()
+        {
+            Assert.That(_singlePostcodeService.ResponseContent["status"].ToString(), Is.EqualTo("200"));
+        }
+
+        [Test]
+        public void CorrectPostCodeIsReturned()
+        {
+            var result = _singlePostcodeService.ResponseContent["result"]["postcode"].ToString();
+            Assert.That(result, Is.EqualTo("EC2Y 5AS"));
+        }
+    }
+}
---
+++ API_TestFramework/API_App/SinglePostcodeService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Threading.Tasks;
+
+namespace API_App
+{
+    public class SinglePostcodeService
+    {
+        #region Properties
+        // Restsharp object which handles communicaiton with the API
+        public RestClient Client;
+        // Restsharp response object
+        public IRestResponse RestResponse { get; set; }
+        // A Newtonsoft object representing the JSON response
+        public JObject ResponseContent;
+        // the postcode used in this API request
+        public string PostcodeSelected { get; set; }
+        #endregion
+
+        // constructor - creates the RestClient object
+        public SinglePostcodeService()
+        {
+            Client = new RestClient { BaseUrl = new Uri(AppConfigReader.BaseUrl) };
+        }
+
+        public void MakeRequest (string postcode)
+        {
+            // set up the request
+            var request = new RestRequest();
+            request.AddHeader("Content-Type", "application/json");
+            PostcodeSelected = postcode;
+
+            // define request resource path, changing to lowercase and removing whitespace
+            request.Resource = $"postcodes/{postcode.ToLower().Replace(" ", "")}";
+
+            // make request
+            RestResponse = Client.Execute(request);
+
+            // parse json into a JObject
+            ResponseContent = JObject.Parse(RestResponse.Content);
+        }
+    }
+}
